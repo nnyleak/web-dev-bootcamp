08ed8b90029a366392d5405bad00e9b8ebdf13c9
Kaelynn Amodia <89933890+nnyleak@users.noreply.github.com>
2025-09-05 11:44:51 -0400

    pushed whatever i did last time

--- capstone projects/capstone 2 - blog web app/index.js
+++ capstone projects/capstone 2 - blog web app/index.js
@@ -9,30 +9,73 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const allPosts = [];
 
+let allPosts = [];
+
+// all post functions
+function Post(title, content) {
+  this.title = title;
+  this.content = content;
+}
+
+function addPost(title, content) {
+  let post = new Post(title, content);
+  allPosts.push(post);
+}
+
+function editPost(index, title, content) {
+  allPosts[index] = new Post(title, content);
+}
+
+function deletePost(index) {
+  allPosts.splice(index, 1);
+}
+
+// middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// paths
+
+// home page
 app.get("/", (req, res) => {
   res.render("index.ejs", { allPosts });
 });
 
+// create post page
 app.get("/create", (req, res) => {
   res.render("create.ejs");
 });
 
-app.get("/edit", (req, res) => {
-    res.render("edit.ejs");
-})
-
-app.post("/submit", (req, res) => {
-  allPosts.push({
-    title: req.body["postTitle"],
-    content: req.body["postContent"],
+// view post page
+app.get("/view/:id", (req, res) => {
+  let index = req.params.id;
+  let post = allPosts[index];
+  res.render("view.ejs", {
+    postId: index,
+    postTitle: post.title,
+    postContent: post.content,
   });
+});
+
+// add post
+app.post("/submit", (req, res) => {
+  let post = new Post(req.body["postTitle"], req.body["postContent"]);
+  allPosts.push(post);
 
   res.render("index.ejs", { allPosts });
+});
+
+// edit post
+app.post("/edit", (req, res) => {
+  console.log("edit request sent");
+
+});
+
+// delete post
+app.post("/delete", (req, res) => {
+    deletePost(req.body)
+  res.redirect("/");
 });
 
 app.listen(port, () => {
