4fe08f2c717e809dfb324e632dd0dd02d523de8f
nnyleak <89933890+nnyleak@users.noreply.github.com>
2025-10-24 12:09:59 -0400

    more work with databases

--- section 33 - postgresql/8.2 Postgres Read/index.js
+++ section 33 - postgresql/8.2 Postgres Read/index.js
@@ -1,8 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'world',
+  password: 'Kjraxoxo_3',
+  port: 5432
+})
+
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error(`error executing query`, err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+})
 
 let totalCorrect = 0;
 
@@ -24,7 +46,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
--- section 33 - postgresql/8.3 Travel Tracker/index.js
+++ section 33 - postgresql/8.3 Travel Tracker/index.js
@@ -1,5 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Kjraxoxo_3",
+  port: 5432,
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -7,8 +18,26 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let visitedCountries = [];
+let countryCodes = [];
+
+db.query("SELECT country_code FROM visited_countries", (err, res) => {
+    if (err) {
+      console.error(`error executing query`, err.stack);
+    } else {
+      res.rows.forEach((country) => {
+        visitedCountries.push(country.country_code);
+      });
+    }
+    db.end();
+  });
+
 app.get("/", async (req, res) => {
   //Write your code here.
+  res.render(`index.ejs`, {
+    countries: visitedCountries,
+    total: visitedCountries.length
+  });
 });
 
 app.listen(port, () => {
